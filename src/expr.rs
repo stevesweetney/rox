3b0a5f47743faab43c3f0a2255735052ab09cacd
Steve Sweetney <stevesweetney@gmail.com>
2019-07-29 15:11:04 -0400

    Add test for `print_ast`

--- src/expr.rs
+++ src/expr.rs
@@ -64,4 +64,31 @@ mod print {
         res.push(')');
         res
     }
+
+    #[cfg(test)]
+    mod test {
+        use super::{print_ast, Expr};
+        use crate::expr::LiteralValue;
+        use crate::token::{Token, TokenType};
+
+        #[test]
+        fn test_print_ast() {
+            let minus_operator = Token::new(TokenType::Minus, 1);
+            let mul_operator = Token::new(TokenType::Star, 1);
+            let lit_123 = LiteralValue::Number(123.0);
+            let lit_4567 = LiteralValue::Number(45.67);
+            let expr = Expr::Binary {
+                left: Box::new(Expr::Unary {
+                    operator: minus_operator,
+                    operand: Box::new(Expr::Literal(lit_123)),
+                }),
+                operator: mul_operator,
+                right: Box::new(Expr::Grouping {
+                    expr: Box::new(Expr::Literal(lit_4567)),
+                }),
+            };
+
+            assert_eq!(print_ast(&expr), "(* (- 123) (grouping 45.67))")
+        }
+    }
 }
